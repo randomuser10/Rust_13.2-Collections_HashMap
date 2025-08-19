99afda9575645cf9051f8dbc0e95ed2ecbc90c64
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-08-19 15:08:15 +0530

    Hashmap update

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,37 @@ fn main() {
     //logging the values to the console
     println!("{:?}", scores);
 
+    //adding a value if not present
+    scores.entry(String::from("Purple")).or_insert(80);
+    scores.entry(String::from("Yellow")).or_insert(90);
+    println!("{scores:?}");
+
     //outputing the values in loop
     for (keys, values) in &scores{
         println!("{keys}: {values}");
     };
+
+    // Overwriting a Value
+
+    scores.insert(String::from("Green"), 30);
+    scores.insert(String::from("Green"), 45);
+    println!("{scores:?}");
+
+    let field_name = String::from("Fav color");
+    let field_value = String::from("Blue");
+
+    let mut new_value = HashMap::new();
+    new_value.insert(field_name, field_value);
+    println!("{new_value:?}");
+
+    // Updating a Value Based on the Old Value
+    let text = "Hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    };
+    println!("{map:?}");
+
 }
---
+++ target/debug/deps/collections_hash_maps-80a2ffaedfc8e46a.d
@@ -0,0 +1,5 @@
+/Users/samayura/Rust/collections_hash_maps/target/debug/deps/collections_hash_maps-80a2ffaedfc8e46a.d: src/main.rs
+
+/Users/samayura/Rust/collections_hash_maps/target/debug/deps/libcollections_hash_maps-80a2ffaedfc8e46a.rmeta: src/main.rs
+
+src/main.rs:

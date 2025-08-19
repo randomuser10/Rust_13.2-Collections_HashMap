d0e8f33511ce09f44c544cd7a4eaea1921caf0bd
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-08-19 14:34:28 +0530

    Adding comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use::std::collections::HashMap;
 fn main() {
+    // creating a mutable collection using HashMap
     let mut scores = HashMap::new();
+
+    //Inserting the keys and value to the collection
 
     scores.insert(String::from("Blue"),10);
     scores.insert(String::from("Yellow"),50);
-
+    
+    //logging the values to the console
     println!("{:?}", scores);
+
+    //outputing the values in loop
+    for (keys, values) in &scores{
+        println!("{keys}: {values}");
+    };
 }
